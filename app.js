b8f81253af61bb450b9200e1a502008433f4622a
zhaoxuetong <zhaoxuetong@chinahomu.com>
2017-02-03 17:52:22 +0800

    a little change

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ console.log('imooc started on port ' + port);
 
 //index page
 app.get('/', function (req, res) {
-    Movie.fetch(function(err,movies){
-        if(err){
+    Movie.fetch(function (err, movies) {
+        if (err) {
             console.log(err)
         }
         res.render('index', {
@@ -35,13 +35,13 @@ app.get('/', function (req, res) {
 
 //detail page
 app.get('/movie/:id', function (req, res) {
-    var id=req.params.id;
-    Movie.findById(id,function(err,movie){
-        if(err){
+    var id = req.params.id;
+    Movie.findById(id, function (err, movie) {
+        if (err) {
             console.log(err)
         }
         res.render('detail', {
-            title: 'imooc '+movie.title,
+            title: 'imooc ' + movie.title,
             movie: movie
         })
     });
@@ -65,41 +65,73 @@ app.get('/admin/movie', function (req, res) {
     })
 });
 
-//admin post movie
-app.post('/admin/movie/new',function(req,res){
-   var id = req.body.movie._id ;
-   var movieObj = req.body.movie;
-   var _movie;
+//admin update movie
+app.get('/admin/update/:id', function (req, res) {
+    var id = req.params.id;
 
-   if(id!=='undefined'){
-       Movie.findbyId(id,function(err,movie){
-           if(err){
-               console.log(err)
-           }
-
-           _movie =
-       })
-   }
+    if (id) {
+        Movie.findById(id, function (err, movie) {
+            res.render('admin',{
+                title:'imooc 后台更新页面',
+                movie:movie
+            })
+        })
+    }
 });
 
+//admin post movie
+app.post('/admin/movie/new', function (req, res) {
+    var id = req.body.movie._id;
+    var movieObj = req.body.movie;
+    var _movie;
+
+    if (id !== 'undefined') {
+        Movie.findbyId(id, function (err, movie) {
+            if (err) {
+                console.log(err)
+            }
+
+            _movie = _.extend(movie, movieObj);
+            _movie.save(function (err, movie) {
+                if (err) {
+                    console.log(err)
+                }
+
+                res.redirect('/movie' + movie._id)
+            })
+        })
+    } else {
+        _movie = new Movie({
+            doctor: movieObj.doctor,
+            title: movieObj.titleyear,
+            country: movieObj.country,
+            language: movieObj.language,
+            year: movieObj.year,
+            poster: movieObj.poster,
+            summary: movieObj.summary,
+            flash: movieObj.flash
+        })
+
+        _movie.save(function (err, movie) {
+            if (err) {
+                console.log(err)
+            }
+
+            res.redirect('/movie/' + movie._id)
+        })
+    }
+});
 
 
 //list page
 app.get('/admin/list', function (req, res) {
-    res.render('list', {
-        title: 'imooc 列表页',
-        movies:[
-            {
-                title: '机械战警',
-                _id: 1,
-                doctor: '何塞·帕蒂利亚',
-                country: '美国',
-                year: 2014,
-                poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
-                language: '英语',
-                flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
-                summary: '《机械战警》是由何塞·帕迪里亚执导，乔尔·金纳曼、塞缪尔·杰克逊、加里·奥德曼等主演的一部科幻电影，改编自1987年保罗·范霍文执导的同名电影。影片于2014年2月12日在美国上映，2014年2月28日在中国大陆上映。影片的故事背景与原版基本相同，故事设定在2028年的底特律，男主角亚历克斯·墨菲是一名正直的警察，被坏人安装在车上的炸弹炸成重伤，为了救他，OmniCorp公司将他改造成了生化机器人“机器战警”，代表着美国司法的未来。 '
-            }
-        ]
-    })
+    Movie.fetch(function (err, movies) {
+        if (err) {
+            console.log(err)
+        }
+        res.render('list', {
+            title: 'imooc 列表页',
+            movies: movies
+        })
+    });
 });
