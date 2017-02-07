a0ec97b5edbe52acb4cc258d5ce4726e6008e1a6
zhaoxuetong <zhaoxuetong@chinahomu.com>
2017-02-07 17:58:02 +0800

    a little change

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 var express = require('express');
+var app = express();
+
 var path = require('path');
 var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 var mongoose = require('mongoose');
-var morgan=require('morgan');
-var logger=morgan(':method :url :status');
-
-var port = process.env.PORT || 3000;
-var app = express();
-var dbUrl='mongodb://localhost/imooc';
-
 mongoose.connect(dbUrl);
 mongoose.connection.on('connected', function () {
     console.log('Connection success!')
 });
 
+var morgan=require('morgan');
+var logger=morgan(':method :url :status');
+
+var port = process.env.PORT || 3000;
+
+var dbUrl='mongodb://localhost/imooc';
+
+
+
 app.set('views', './app/views/pages');
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 app.locals.moment = require('moment');
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+
 
 var cookieParser = require('cookie-parser');
 app.use(cookieParser());
