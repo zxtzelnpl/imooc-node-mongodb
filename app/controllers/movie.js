248d4fb73ddadeb217e0a5e1d434a2a75dcbd3a9
zxtzelnpl <zhaoxuetong@chinahomu.com>
2017-02-07 23:43:39 +0800

    The first stage has finished

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -94,19 +94,34 @@ exports.save = function (req, res) {
         })
     } else {
         _movie = new Movie(movieObj);
+        console.log(_movie);
         var categoryId = _movie.category;
+        var categoryName = movieObj.categoryName;
         _movie.save(function (err, movie) {
             if (err) {
                 console.log(err)
             }
-            Category.findById(categoryId,function(err,category){
-                category.movies.push(movie._id);
 
+            console.log(categoryId);
+            console.log(categoryName);
+
+            if(categoryId){
+                Category.findById(categoryId,function(err,category){
+                    category.movies.push(movie._id);
+
+                    category.save(function(err,category){
+                        res.redirect('/movie/' + movie._id)
+                    })
+                });
+            }else if(categoryName){
+                var category = new Category({
+                    name:categoryName,
+                    movies:[movie._id]
+                });
                 category.save(function(err,category){
                     res.redirect('/movie/' + movie._id)
-                })
-            });
-
+                });
+            }
         })
     }
 };
